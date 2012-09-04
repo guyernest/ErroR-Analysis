81e33d503eaa079aaed8414503b8c654606ca761
Guy Ernest <guy@newbrandanalytics.com>
2012-09-04 21:51:24 +0300

    Adding total recall and precision lines

--- CategoryBreakdownPR.R
+++ CategoryBreakdownPR.R
@@ -2,10 +2,8 @@
 setwd("/path/to/files/")
 
 # Load a predefine file
-#cats <- read.table("/path/to/files/fileName.tsv", header=FALSE, sep="\t")
 #cats <- read.table("/path/to/files/fileName.tsv", header=FALSE, sep="\t", comment.char = "", , quote = "")
 # Or choose the file to load
-cats <- read.table(file.choose(), header=FALSE, sep="\t")
 cats <- read.table(file.choose(), header=FALSE, sep="\t", comment.char = "", quote = "")
 
 # Adding Match counter (0|1) based on matching rule (it can be also comparing V3 and V4)
@@ -15,6 +13,9 @@ cats$Match[cats$V2=='false'] <- 0
 # Adding Significant counter (0|1) based on matching rule (it can be if V4 is null)
 cats$Sig[cats$V1=='1.0'] <- 1.0
 cats$Sig[cats$V1=='null'] <- 0
+
+recall <- mean(cats$Match)
+precision <- sum(cats$Match)/sum(cats$Sig)
 
 library(plyr)
 humanMeans <- ddply(cats,~V3,summarise,recall=mean(Match),precision=sum(Match)/sum(Sig),nrow=length(V3))
@@ -33,8 +34,10 @@ library(scales)
 ggplot(humanMeans, aes(y=recall, x=reorder(V3,nrow), size=20)) + 
   # The recall symbols
   geom_point(colour="#FF9999", shape = '+', size = 15) + 
+  geom_hline(yintercept=recall, colour="#FF9999") +
   # The percision symbols
   geom_point(aes(y=precision, x=reorder(V3,nrow)), shape = '+', size=15) + 
+  geom_hline(yintercept=precision) +
   # Chart Title
   opts(title="Precision and Recall per Categories") + 
   # X-Axis Titles
