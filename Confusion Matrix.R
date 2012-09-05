8042b585c3fc89a4b19fee48d7d13b7eace1e4e8
Guy Ernest <guy@newbrandanalytics.com>
2012-09-05 16:48:03 +0300

    Adding count to non zero tiles

--- Confusion Matrix.R
+++ Confusion Matrix.R
@@ -17,6 +17,10 @@ confMatrix <- function(data, human, guess) {
   #calculate percentage of test cases based on actual frequency
   confusion <- merge(confusion, actual, by=c("Actual"))
   confusion$Percent <- confusion$Freq/confusion$ActualFreq*100
+  
+  # Sorting by frequency
+  confusion <- confusion[with(confusion,order(-ActualFreq)), ]
+  
   return (confusion)
 }  
 
@@ -31,13 +35,13 @@ library(ggplot2)
 tile <- ggplot() + geom_tile(aes(x=Actual, y=Predicted,fill=Percent),data=confusion, color="black",size=0.1) + labs(x="Actual",y="Predicted")
 
 # next we render text values. If you only want to indicate values greater than zero then use data=subset(confusion, Percent > 0)
-tile <- tile + geom_text(aes(x=Actual,y=Predicted, label=sprintf("%.1f", Percent)),data=confusion, size=3, colour="black") + scale_fill_gradient(low="grey",high="red")
+tile <- tile + geom_text(aes(x=Actual,y=Predicted, label=sprintf("%.1f", Percent)),data=subset(confusion, Percent > 0), colour="black", size=4, vjust = -0.2) + 
+  scale_fill_gradient(name = "% of Actual", low="grey",high="red")
+
+tile <- tile + geom_text(aes(x=Actual,y=Predicted, label=Freq),data=subset(confusion, Freq > 0), colour="white", size=3, vjust = 1.2) 
 
 # lastly we draw diagonal tiles. We use alpha = 0 so as not to hide previous layers but use size=0.3 to highlight border
-tile <- tile + geom_tile(aes(x=Actual,y=Predicted),data=subset(confusion, as.character(Actual)==as.character(Predicted)), color="black",size=0.3, fill="black", alpha=0)
+#tile <- tile + geom_tile(aes(x=Actual,y=Predicted),data=subset(confusion, as.character(Actual)==as.character(Predicted)), color="black",size=0.3, fill="black", alpha=0)
 
 tile + opts(axis.text.x=theme_text(angle=90))
-
-#render
-tile
 
